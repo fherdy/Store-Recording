70d56be418b58d5839c22b597c9ce56fd0d887c7
ferdinand <paa1000@yahoo.com>
2019-09-08 22:52:30 -0400

    Context.js file undated to handle product display

--- src/Context.js
+++ src/Context.js
@@ -1,15 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {storeProducts, detailProduct} from './data';
 
 const ProductContext= React.createContext();
 
 
 class ProductProvider extends Component {
+    state ={
+        products:storeProducts,
+        detailProduct:detailProduct
+    }
+    handleDetail=()=>{
+        console.log('Hello from Detail');
+    }
+    addToCart=()=>{
+        console.log('Hello from add to Cart');
+    }
     render() {
         return (
-            <ProductContext.Provider value="Hello from context">
+            <ProductContext.Provider value={{
+                ...this.state,
+                handleDetail:this.handleDetail,
+                addToCart:this.addToCart
+                }}>
                 {this.props.children}
             </ProductContext.Provider>
-        )
+        );
     }
 }
 
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,38 +1,36 @@
 import React, { Component } from 'react';
-import product from './Product';
+import Product from './Product';
 import Title from './Title';
 import {storeProducts} from '../data';
-import {ProductConsumer} from '../Context'
+import {ProductConsumer} from '../context';
 
 export default class ProductList extends Component {
 
     state={
-        product:storeProducts
+        products: storeProducts
         
-    }
+    };
 
 
 
     render() {
-        console.log(this.state.product)
         return (
             <React.Fragment>
                 <div className="py-5">
                     <div className="container">
                         <Title name ="our" title="products"/>
-                    <div className="row">
-                        <ProductConsumer value="Hello from context">
-                            {value => {
-                                return<h1>{value}</h1>;
-                            }}
-                        </ProductConsumer>
-
-
-
-                </div>
-                </div>
+                            <div className="row">
+                                <ProductConsumer>
+                                {  value => {
+                                    return value.products.map(product =>{
+                                        return <Product key={product.id} product={product}/>;
+                                    });
+                                } } 
+                            </ProductConsumer> 
+                        </div>
+                    </div>
                 </div>
                 </React.Fragment>
-        )
+        );
     }
 }
