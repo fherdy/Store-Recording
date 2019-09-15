c9d8b1068560d4b6bf1e059bfe8939911847e118
ferdinand <paa1000@yahoo.com>
2019-09-15 00:56:13 -0400

    PropTypes imorted

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import {ProductConsumer} from "../context";
+import PropTypes from 'prop-types';
 
 export default class Product extends Component {
     render() {
@@ -38,6 +39,16 @@ export default class Product extends Component {
         );
     }
 }
+
+Product.prototypes={
+    product:PropTypes.shape({
+        id:PropTypes.number,
+        img:PropTypes.string,
+        price:PropTypes.number,
+        inCart:PropTypes.bool
+    }).isRequired
+}
+
  const ProductWrapper=styled.div `
 .card{
     border-color:transparent;
